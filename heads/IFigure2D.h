13df558c31f6a22137b86e1231d87c773b5df175
dimandry-ee <dmitry.a.salnikov@gmail.com>
2026-04-13 00:22:42 +0300

    Ï2D

--- heads/IFigure2D.h
+++ heads/IFigure2D.h
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 #pragma once
-=======
->>>>>>> e90e69a6dd267d1b4e57e05a1b463f178397a749
 #include<iostream>
 
 using namespace std;
 
 class IFigure2D{
 public:
-<<<<<<< HEAD
   virtual void S() = 0;
   virtual void P() = 0;
-=======
-  virtual void square() = 0;
-  virtual void perimeter() = 0;
->>>>>>> e90e69a6dd267d1b4e57e05a1b463f178397a749
 };
