64fa5214b60774ed7d4c439dedfb34854afb91c4
Bogdan550 <dogfog2021@gmail.com>
2026-04-14 00:12:34 +0300

    factory2

---
+++ heads/factory2.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <memory>
+#include <iostream>
+#include "circle.cpp"
+#include "triangle.cpp"
+#include "trapezoid.cpp"
+#include "polygon.cpp"
+#include "rectangle.cpp"
+// #include "sphere.cpp"
+// #include "cube.cpp"
+using namespace std;
+
+enum class figure2 {cir, tri, trap, polyg, rectan};
+
+class Factory{
+public:
+    std::unique_ptr<IFigure2D> create(figure2 a);
+};
---
+++ src/factory2.cpp
@@ -0,0 +1,22 @@
+#include"factory.h"
+
+std::unique_ptr<IFigure2D> Factory::create(figure2 a){
+     switch(a){
+            case figure2::cir:
+                return make_unique<circle>();
+                break;
+            case figure2::tri:
+                return make_unique<triangle>();
+                break;
+            case figure2::trap:
+                return make_unique<trapezoid>();
+                break;
+            case figure2::polyg:
+                return make_unique<polygon>();
+                break;
+            case figure2::rectan:
+                return make_unique<rectangle>();
+                break;
+        }
+        return nullptr;
+}
